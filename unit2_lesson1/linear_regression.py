5de177b3324585f3839d4604d7566ded0b4741d5
elpenao <elpenao@gmail.com>
2015-03-30 20:58:09 -0400

    P values, R2

--- unit2_lesson1/linear_regression.py
+++ unit2_lesson1/linear_regression.py
@@ -1,6 +1,6 @@
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from pandas.tools.plotting import scatter_matrix
+import statsmodels.api as sm
 
 loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 
@@ -17,17 +17,23 @@ cleanFico = cleanFico.map(lambda z:[int(n) for n in z])
 simpleFico = [score[0] for score in cleanFico]
 loansData['FICO.Score'] = simpleFico
 
-# print loansData['FICO.Score'][0:10]
+intrate = loansData['Interest.Rate']
+loanamt = loansData['Amount.Requested']
+fico = loansData['FICO.Score']
 
-plt.figure()
-p = loansData['FICO.Score'].hist()
-plt.show()
+# The dependent variable
+y = np.matrix(intrate).transpose()
+# The independent variables shaped as columns
+x1 = np.matrix(fico).transpose()
+x2 = np.matrix(loanamt).transpose()
 
-plt.figure()
-a = pd.scatter_matrix(loansData, alpha=0.05, figsize=(10,10))
-plt.show()
+x = np.column_stack([x1,x2])
 
-plt.figure()
-a = pd.scatter_matrix(loansData, alpha=0.05, figsize=(10,10), diagonal='hist')
-plt.show()
+X = sm.add_constant(x)
+model = sm.OLS(y,X)
+f = model.fit()
 
+print 'Coefficients: ', f.params[0:2]
+print 'Intercept: ', f.params[2]
+print 'P-Values: ', f.pvalues
+print 'R-Squared: ', f.rsquared
