5d11c2cb7360c73bcbac0f345ba17d67674353a8
elpenao <elpenao@gmail.com>
2015-04-21 20:44:01 -0400

    v2

--- unit2_lesson1/multivariate.py
+++ unit2_lesson1/multivariate.py
@@ -10,21 +10,39 @@ cleanInterestRate = df['int_rate'].map(lambda x: round(float(x.rstrip('%')) / 10
 df['int_rate'] = cleanInterestRate
 
 df['home_own'] = 0
-df.loc[('home_ownership' == "OWN"), 'home_own'] = 1
+df.ix[df.home_ownership=="OWN", 'home_own'] = 1
+df['rent'] = 0
+df.ix[df.home_ownership=="RENT", 'rent'] = 1
+df['mortgage'] = 0
+df.ix[df.home_ownership=="MORTGAGE", 'mortgage'] = 1
+df['none'] = 0
+df.ix[df.home_ownership=="NONE", 'none'] = 1
+df['other'] = 0
+df.ix[df.home_ownership=="OTHER", 'other'] = 1
+
 
 intrate = df['int_rate'].fillna(0)
 annual_inc = df['annual_inc'].fillna(0)
-home_own = df['home_own']
-
-print home_own[:200]
+home_own = df['home_own'].all()
+rent = df['rent'].all()
+mortgage = df['mortgage'].all()
+none = df['none'].all()
+other = df['other'].all()
 
 # The dependent variable
 y = np.matrix(intrate).transpose()
 # The independent variables shaped as columns
 x1 = np.matrix(annual_inc).transpose()
 x2 = np.matrix(home_own).transpose()
+x3 = np.matrix(rent).transpose()
+x4 = np.matrix(mortgage).transpose()
+x5 = np.matrix(none).transpose()
+x6 = np.matrix(other).transpose()
 
-X = sm.add_constant(x1,x2)
+# x = np.column_stack((x1,x2))
+x = np.concatenate((x1,x2))
+
+X = sm.add_constant(x)
 est = sm.OLS(y, X).fit()
 
 print est.summary()
